0177106c7f194219d4bfc31acf4f1279275430b3
brown <kconly@gmil.com>
2020-07-02 18:07:19 +0900

    아이고 머리야2

--- get_stock_new.py
+++ get_stock_new.py
@@ -5,50 +5,49 @@ import parmap
 import itertools
 import sqlite3
 
-def get_frgn(code, page):
+def get_price(code, page):
     try:
-        url_frgn = 'https://finance.naver.com/item/frgn.nhn?code=' + code
-        pg_url = '{url}&page={page}'.format(url=url_frgn, page=page)
-        frgn = pd.read_html(pg_url, header=1, encoding='euc-kr')[2].dropna()
+        url_price = 'https://finance.naver.com/item/sise_day.nhn?code=' + code
+        pg_url = '{url}&page={page}'.format(url=url_price, page=page)
+        price = pd.read_html(pg_url, header=0)[0].dropna()
 
-        frgn1 = frgn.set_index("날짜")
+        # price = price.set_index("날짜")
 
-        edit = lambda x: float(x.replace('%', ''))
-        edit2 = lambda x: float(x.replace(',', ''))
-
-        data = pd.DataFrame()
-        data['등락률'] = frgn1['등락률'].apply(edit)
-        data['기관순매매'] = frgn1['순매매량'].apply(edit2)
-        data['외국인순매매'] = frgn1['순매매량.1'].apply(edit2)
-        data['외국인보유주수'] = frgn1['보유주수']
-        data['외국인보유율'] = frgn1['보유율'].apply(edit)
-
-        con = sqlite3.connect('./Json/stock_info.db')
-        data.to_sql(code, con, if_exists='append')
+        con = sqlite3.connect('./Json/stock_price.db')
+        price.to_sql(code, con, if_exists='append')
 
     except:
+        print(code, "에러났데")
         pass
 
+# get_price('037070', 1000)
 
-def append_frgn():
 
-    codes = pd.read_excel('코드리스트2.xlsx', converters={'종목코드':str})['종목코드']
-    pages = range(1,200)
+def append_price():
+
+    # codes = pd.read_excel('코드리스트2.xlsx', converters={'종목코드':str})['종목코드']
+    codes = ['037070']
+    pages = range(1,1000)
 
     input_list = list(itertools.product(codes, pages))
-    parmap.starmap(get_frgn, input_list, pm_pbar=True)
+    parmap.starmap(get_price, input_list, pm_pbar=True)
 
 def drop_duplicates(code):
-    con = sqlite3.connect('./Json/stock_info_test.db')
+    con = sqlite3.connect('./Json/stock_price.db')
     data = pd.read_sql("SELECT * FROM" + " " + "'" + code + "'", con)
     data = data.drop_duplicates(['날짜'], keep='first')
     data = data.set_index('날짜').sort_index(ascending=False)
+    del data['level_0']
+    del data['index']
     data.to_sql(code, con, if_exists='replace')
 
 def drop_all():
-    codes = pd.read_excel('코드리스트2.xlsx', converters={'종목코드': str})['종목코드']
+    # codes = pd.read_excel('코드리스트2.xlsx', converters={'종목코드': str})['종목코드']
+    codes = ['037070']
     parmap.map(drop_duplicates, codes, pm_pbar=True)
 
 if __name__ == "__main__":
-    append_frgn()
+    append_price()
     drop_all()
+
+# drop_duplicates('037070')
