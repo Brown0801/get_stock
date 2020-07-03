95c4433e69b4e8ff915b766b14a080e5065ee93f
brown <kconly@gmil.com>
2020-07-03 15:48:34 +0900

    아이고 머리야3

--- get_stock_new.py
+++ get_stock_new.py
@@ -1,24 +1,25 @@
 import pandas as pd
-import requests
-from bs4 import BeautifulSoup
 import parmap
 import itertools
 import sqlite3
 
 def get_price(code, page):
-    try:
-        url_price = 'https://finance.naver.com/item/sise_day.nhn?code=' + code
-        pg_url = '{url}&page={page}'.format(url=url_price, page=page)
-        price = pd.read_html(pg_url, header=0)[0].dropna()
+    # try:
+    url_price = 'https://finance.naver.com/item/sise_day.nhn?code=' + code
+    pg_url = '{url}&page={page}'.format(url=url_price, page=page)
+    price = pd.read_html(pg_url, header=0)[0].dropna()
 
-        # price = price.set_index("날짜")
+    # con = sqlite3.connect('./Json/stock_price.db')
+    # price.to_sql(code, con, if_exists='append')
 
-        con = sqlite3.connect('./Json/stock_price.db')
-        price.to_sql(code, con, if_exists='append')
+    path = "./temp/" + str(code) + "price" + str(page) + ".xlsx"
 
-    except:
-        print(code, "에러났데")
-        pass
+    price.to_excel(path)
+
+
+    # except:
+    #     print(code, page, "에러났데")
+    #     pass
 
 # get_price('037070', 1000)
 
@@ -26,28 +27,35 @@ def get_price(code, page):
 def append_price():
 
     # codes = pd.read_excel('코드리스트2.xlsx', converters={'종목코드':str})['종목코드']
-    codes = ['037070']
+    codes = ['009810']
     pages = range(1,1000)
 
     input_list = list(itertools.product(codes, pages))
     parmap.starmap(get_price, input_list, pm_pbar=True)
 
-def drop_duplicates(code):
-    con = sqlite3.connect('./Json/stock_price.db')
-    data = pd.read_sql("SELECT * FROM" + " " + "'" + code + "'", con)
+def sum_price(code):
+    print(code, 'start')
+    data = pd.DataFrame()
+    for page in range(1,999):
+        path = "./temp/" + str(code) + "price" + str(page) + ".xlsx"
+        data1 = pd.read_excel(path)
+        data = data.append(data1)
+
     data = data.drop_duplicates(['날짜'], keep='first')
     data = data.set_index('날짜').sort_index(ascending=False)
-    del data['level_0']
-    del data['index']
+    del data['Unnamed: 0']
+
+    con = sqlite3.connect('./Json/stock_price.db')
     data.to_sql(code, con, if_exists='replace')
 
-def drop_all():
-    # codes = pd.read_excel('코드리스트2.xlsx', converters={'종목코드': str})['종목코드']
-    codes = ['037070']
-    parmap.map(drop_duplicates, codes, pm_pbar=True)
+    print(code, 'end')
+
+# sum_price('009810')
+
 
 if __name__ == "__main__":
     append_price()
-    drop_all()
 
-# drop_duplicates('037070')
+codes = pd.read_excel('코드리스트2.xlsx', converters={'종목코드':str})['종목코드']
+for code in codes:
+    sum_price(code)
