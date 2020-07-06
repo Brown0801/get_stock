9fe181e0c066c56362b3cdcc8f1bfb7effa036b6
brown <kconly@gmil.com>
2020-07-06 15:22:53 +0900

    아이고 머리야5

--- get_stock_new.py
+++ get_stock_new.py
@@ -12,9 +12,10 @@ def get_price(code, page):
     # con = sqlite3.connect('./Json/stock_price.db')
     # price.to_sql(code, con, if_exists='append')
 
-    path = "./temp/" + str(code) + "price" + str(page) + ".xlsx"
+    path = "./temp/" + str(code) + ".db"
 
-    price.to_excel(path)
+    con = sqlite3.connect(path)
+    price.to_sql('price', con, if_exists='append')
 
 
     # except:
@@ -26,37 +27,45 @@ def get_price(code, page):
 
 def append_price():
 
-    # codes = pd.read_excel('코드리스트2.xlsx', converters={'종목코드':str})['종목코드']
-    codes = ['095570']
+    codes = pd.read_excel('코드리스트_test.xlsx', converters={'종목코드':str})['종목코드']
+    # codes = ['095570']
     pages = range(1,1000)
 
-    input_list = list(itertools.product(codes, pages))
-    parmap.starmap(get_price, input_list, pm_pbar=True)
+    for code in codes:
+        csv = pd.DataFrame(columns=['날짜', '종가', '전일비', '시가', '고가', '저가', '거래량'])
+        path = "./temp/" + str(code) + ".db"
+        con = sqlite3.connect(path)
+        csv.to_sql('price', con, if_exists='replace')
+
+        input_list = list(itertools.product([code], pages))
+        parmap.starmap(get_price, input_list, pm_pbar=True)
 
 def sum_price(code):
-    print(code, 'start')
-    data = pd.DataFrame()
-    for page in range(1,999):
-        path = "./temp/" + str(code) + "price" + str(page) + ".xlsx"
-        data1 = pd.read_excel(path)
-        data = data.append(data1)
+    # print(code, 'start')
+    path = "./temp/" + str(code) + "price" + ".csv"
+    data = pd.read_csv(path)
 
     data = data.drop_duplicates(['날짜'], keep='first')
-    data = data.set_index('날짜').sort_index(ascending=False)
-    del data['Unnamed: 0']
+    # data = data.set_index('날짜').sort_index(ascending=False)
+    #
+    print(data)
 
-    con = sqlite3.connect('./Json/stock_price.db')
-    data.to_sql(code, con, if_exists='replace')
 
-    print(code, 'end')
 
-# sum_price('009810')
+    # del data['Unnamed: 0']
+    #
+    # con = sqlite3.connect('./Json/stock_price.db')
+    # data.to_sql(code, con, if_exists='replace')
+    #
+    # print(code, 'end')
+
+# sum_price('095570')
 
 
 if __name__ == "__main__":
     append_price()
-
-    # codes = pd.read_excel('코드리스트2.xlsx', converters={'종목코드':str})['종목코드']
-    codes = ['095570']
-    for code in codes:
-        sum_price(code)
+#
+#     # # codes = pd.read_excel('코드리스트2.xlsx', converters={'종목코드':str})['종목코드']
+#     # codes = ['095570']
+#     # for code in codes:
+#     #     sum_price(code)
---
+++ get_stock_new2.py
@@ -0,0 +1,93 @@
+import pandas as pd
+import parmap
+import itertools
+import sqlite3
+from tqdm import tqdm
+from datetime import datetime
+
+
+def get_price(code):
+    now = datetime.now()
+
+    try:
+        path = "./temp/" + str(code) + ".db"
+        con = sqlite3.connect(path)
+        price1 = pd.read_sql("SELECT * FROM price", con, index_col="날짜")
+
+
+        #새 데이터 불러오기 (조금만)
+        pages = 3
+        price2 = pd.DataFrame()
+        for page in range(1, pages):
+            url_price = 'https://finance.naver.com/item/sise_day.nhn?code=' + code
+            pg_url = '{url}&page={page}'.format(url=url_price, page=page)
+            price = pd.read_html(pg_url, header=0)[0].dropna()
+            price2 = price2.append(price)
+
+        price2 = price2.set_index(['날짜'])
+
+        if price2.index[0] == now.strftime("%Y.%m.%d"):
+            if now.hour < 16:
+                price2 = price2[1:]
+                price1 = price1.append(price2)
+                price1 = price1.drop_duplicates()
+                price1 = price1.sort_index(ascending=False)
+                price1.to_sql('price', con, if_exists='replace')
+
+            else:
+                price1 = price1.append(price2)
+                price1 = price1.drop_duplicates()
+                price1 = price1.sort_index(ascending=False)
+                price1.to_sql('price', con, if_exists='replace')
+
+        else:
+            price1 = price1.append(price2)
+            price1 = price1.drop_duplicates()
+            price1 = price1.sort_index(ascending=False)
+            price1.to_sql('price', con, if_exists='replace')
+
+
+    except:
+        pages = 1000
+
+        price1 = pd.DataFrame()
+
+        # for page in tqdm(range(1, pages), mininterval=1, desc=code):
+        for page in range(1, pages):
+
+            # try:
+            url_price = 'https://finance.naver.com/item/sise_day.nhn?code=' + code
+            pg_url = '{url}&page={page}'.format(url=url_price, page=page)
+            price = pd.read_html(pg_url, header=0)[0].dropna()
+
+            price1 = price1.append(price)
+
+        price1 = price1.drop_duplicates(['날짜'], keep='first')
+        price1 = price1.set_index(['날짜']).sort_index(ascending=False)
+
+        if price1.index[0] == now.strftime("%Y.%m.%d"):    #조회 시도하는 날짜가 장중
+            if now.hour < 16:    #장 마감 전인가?? : 당일 시세 데이터 제외
+                price1 = price1[1:]
+                path = "./temp/" + str(code) + ".db"
+                con = sqlite3.connect(path)
+                price1.to_sql('price', con, if_exists='replace')
+            else:  #장 마감 후 : 당일 시세 데이터 포함
+                path = "./temp/" + str(code) + ".db"
+                con = sqlite3.connect(path)
+                price1.to_sql('price', con, if_exists='replace')
+        else:  #조회 시도하는 날이 장중이 아닐때
+            path = "./temp/" + str(code) + ".db"
+            con = sqlite3.connect(path)
+            price1.to_sql('price', con, if_exists='replace')
+
+
+
+# get_price('095570', 30)
+
+
+if __name__ == '__main__':
+
+    codes = pd.read_excel('코드리스트2.xlsx', converters={'종목코드': str})
+    code = codes['종목코드']
+
+    parmap.map(get_price, code, pm_pbar=True)
