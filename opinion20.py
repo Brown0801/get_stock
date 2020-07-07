fbbf5e5354e356da88f9888ca2ba39d0057a04f3
brown <kconly@gmil.com>
2020-07-07 18:30:57 +0900

    아이고 머리야6

---
+++ opinion20.py
@@ -0,0 +1,62 @@
+import pandas as pd
+import sqlite3
+
+
+def opinion20(date):
+    kind_wics = pd.read_csv('wics_kind.csv', converters={'코드': str})['wics']
+
+    con = sqlite3.connect('./Json/infos.db')
+
+    for kind in kind_wics:
+        try:
+            df2 = pd.read_sql("SELECT * FROM infos WHERE [업종(WICS)] = " + "'" + kind + "'", con, index_col='종목코드')
+            codes = df2.index
+
+            df3 = pd.DataFrame(columns={'kind', 'code', 'mkt_vol', 'earn', 'max_date', 'trade_vol', 'buy_price', 'max_price'})
+
+            for code in codes:
+                try:
+                    path = "./temp/" + str(code) + ".db"
+                    con1 = sqlite3.connect(path)
+
+                    price = pd.read_sql("SELECT * FROM price", con1, index_col='날짜').sort_index(ascending=True)
+                    infos = pd.read_sql("SELECT * FROM infos", con1)
+
+                    date_idx = price.index.tolist()
+                    idx_num = date_idx.index(date)
+
+                    mkt_vol = price['종가'][idx_num] * infos['상장주식수']
+
+                    price['거래대금'] = price['종가'] * price['거래량']
+
+
+                    trade_vol = price['거래대금'].rolling(window=5).sum()[idx_num]
+
+
+                    price_after = price[idx_num+1 : idx_num +21]
+                    price_max = price_after['고가'].max()
+                    max_date = price_after['고가'].idxmax()
+
+
+                    earn = int(((price_max - price['종가'][idx_num])/price['종가'][idx_num])*100)
+
+                    df4 = pd.DataFrame({'kind': kind, 'code':code, 'mkt_vol':mkt_vol, 'earn':earn, 'max_date':max_date, 'trade_vol':trade_vol, 'buy_price':price['종가'][idx_num], 'max_price':price_max})
+                    df3 = df3.append(df4, ignore_index=True, sort=False)
+
+                except:
+                    # print(kind, code, '데이터없음')
+                    pass
+
+            df3 = df3.set_index('code')
+
+            min = df3['mkt_vol'].idxmin()
+            name = df2['기업명'][min]
+            earn = df3['earn'][min]
+            print(kind, "/", name, "/", min, "/", date, "/", max_date, "/", int(df3['trade_vol'].sum()/100000000), "/", earn, df3['buy']
+
+        except:
+            print(kind, "여긴없데")
+            pass
+
+
+opinion20('2019.01.02')
--- test1111.py
+++ test1111.py
@@ -5,6 +5,7 @@ import parmap
 import itertools
 import sqlite3
 import warnings
+from datetime import datetime
 
 
 def get_wics(code):
@@ -56,100 +57,116 @@ def get_move(code):   #유동주식수 구하기
     except:
         return "N/A"
 
+def get_name(code):   #종목명 구하기
+    url = 'https://finance.naver.com/item/main.nhn?code=' + code
+    html = requests.get(url).text
+    soup = BeautifulSoup(html, "html5lib")
+    tags = soup.select("#middle > div.h_company > div.wrap_company > h2 > a")
+    tag = tags[0].text
+    return tag
 
 
-# def ffff(code):
-#     warnings.filterwarnings(action='ignore')
-#
-#     df1 = pd.read_excel('코드리스트2.xlsx', converters={'종목코드': str})
-#     df1 = df1.set_index('종목코드')
-#
-#     df1['EPS'][code] = get_eps(code)
-#
-#     df2 = df1.loc[code]
-#
-#     df2.to_csv('코드리스트2.csv', header='True', mode='w', encoding='utf-8-sig')
-#
-#
-#
-# def hurry():
-#     df1 = pd.read_excel('코드리스트2.xlsx', converters={'종목코드': str})
-#     codes = df1['종목코드']
-#
-#     parmap.map(ffff, codes, pm_pbar=True)
-#
-#
-#
-#
-# if __name__ == '__main__':
-#     hurry()
-
-
-def to_db():
-    data = pd.read_excel('코드리스트2.xlsx', converters={'종목코드': str, '업종코드' : str})
-    con = sqlite3.connect('./Json/infos.db')
-    data.to_sql('infos', con, if_exists='replace', index=False)
-
-def get_info(code):
-    con = sqlite3.connect('./Json/infos.db')
-    cur = con.cursor()
-
-    eps = str(get_eps(code))
-    bps = str(get_bps(code))
-    move = str(get_move(code))
-    wics = get_wics(code)
-
-    cur.execute("UPDATE infos SET EPS=" + "'" + eps + "'" + "WHERE 종목코드=" + "'" + code + "'")
-    con.commit()
-
-    cur.execute("UPDATE infos SET BPS=" + "'" + bps + "'" + "WHERE 종목코드=" + "'" + code + "'")
-    con.commit()
-
-    cur.execute("UPDATE infos SET 유동주식수=" + "'" + move + "'" + "WHERE 종목코드=" + "'" + code + "'")
-    con.commit()
-
-    cur.execute("UPDATE infos SET '업종(WICS)'=" + "'" + wics + "'" + "WHERE 종목코드=" + "'" + code + "'")
-    con.commit()
-
-    con.close()
-
-# # def execute():
-# #     codes = pd.read_excel('코드리스트2.xlsx', converters={'종목코드': str})['종목코드']
-# #     parmap.map(get_info, codes, pm_pbar=True)
-# #
-# #
-# # if __name__ == '__main__':
-# #     execute()
-#
-# codes = pd.read_excel('코드리스트2.xlsx', converters={'종목코드': str})['종목코드']
-# i = 0
-# all = len(codes)
-# for code in codes:
-#     i = i + 1
-#     print(i, "/", all)
-#     get_info(code)
-
-
-con = sqlite3.connect('./Json/infos.db')
-# df1 = pd.read_sql("SELECT * FROM infos WHERE [업종(WICS)]='화학'", con)
-df1 = pd.read_sql("SELECT * FROM infos", con)
-wics = df1['업종(WICS)'].drop_duplicates()
-
-for kind in wics:
+def get_infos(code):
     try:
-        df2 = pd.read_sql("SELECT * FROM infos WHERE [업종(WICS)]=" + "'" + kind + "'" + "and EPS > 0", con, index_col='종목코드')
-        codes = df2.index
+        infos = pd.DataFrame(columns=['종목명', '종목코드', 'EPS', 'BPS', '유동주식수', 'WICS', '업데이트'])
 
-        con1 = sqlite3.connect('./Json/stocks_price_vol.db')
+        now = datetime.now()
 
-        for code in codes:
-            price = pd.read_sql("SELECT * FROM" + " " + "'" + code + "'", con1, index_col='날짜')['종가']['2020.06.22']
+        name = [get_name(code)]
+        eps = [get_eps(code)]
+        bps = [get_bps(code)]
+        move = [get_move(code)]
+        wics = [get_wics(code)]
+        update = [now.strftime("%Y.%m.%d")]
 
-            df2['시가총액'] = df2['상장주식수(주)'] * price
 
-        min = df2['시가총액'].idxmin()
-        name = df2['기업명'][min]
-        print(kind, min, name)
+        infos['종목명'] = name
+        infos['종목코드'] = [str(code)]
+        infos['EPS'] = eps
+        infos['BPS'] = bps
+        infos['유동주식수'] = move
+        infos['WICS'] = wics
+        infos['업데이트'] = update
+
+        path = "./temp/" + str(code) + ".db"
+        con = sqlite3.connect(path)
+
+        infos.to_sql('infos', con, if_exists='replace')
 
     except:
-        print(kind, code, "여긴없데")
+        print(code, '에러-재시도')
+
+#     print(infos)
+#
+# get_infos('095570')
+
+
+def find_null(code):
+    path = "./temp/" + str(code) + ".db"
+    con = sqlite3.connect(path)
+
+    try:
+        data = pd.read_sql("SELECT * FROM infos", con)
+        name = data['종목명']
+        pass
+
+    except:
+        print(code, '재시도')
+        get_infos(code)
+
+
+def plus_info(code):
+    try:
+        path = "./temp/" + str(code) + ".db"
+        con = sqlite3.connect(path)
+
+        infos = pd.read_sql("SELECT * FROM infos", con)
+
+        data = pd.read_excel('코드리스트2.xlsx', converters={'종목코드': str, '업종코드': str})
+        data = data.set_index('종목코드').loc[code]
+
+        infos['업종코드'] = data['업종코드']
+        infos['업종'] = data['업종']
+        infos['상장주식수'] = data['상장주식수']
+        infos['자본금'] = data['자본금']
+        infos['액면가'] = data['액면가']
+        infos['시장구분'] = data['시장구분']
+
+        infos.to_sql('infos', con, if_exists='replace')
+
+    except:
+        print(code, '에러')
+        pass
+
+# plus_info('095570')
+
+
+def del_trash(code):
+    try:
+        path = "./temp/" + str(code) + ".db"
+        con = sqlite3.connect(path)
+
+        infos = pd.read_sql("SELECT * FROM infos", con)
+        del level_0
+        infos.to_sql('infos', con, if_exists='replace')
+
+    except:
+        pass
+
+# del_trash('095570')
+
+
+if __name__ == '__main__':
+
+    codes = pd.read_excel('코드리스트2.xlsx', converters={'종목코드': str})
+    code = codes['종목코드']
+
+    parmap.map(get_infos, code, pm_pbar=True)
+    parmap.map(find_null, code, pm_pbar=True)
+    parmap.map(plus_info, code, pm_pbar=True)
+    parmap.map(del_trash, code, pm_pbar=True)
+
+
+
+
+
